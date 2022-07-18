47a755161a28ab82eb0b236779073c927b6a64ca
Marquise Williams <marquisewms@example.com>
2022-07-18 16:38:01 -0400

    Finished captcha
    Needs debugging because captcha not showing

---
+++ captcha.js
@@ -0,0 +1,42 @@
+(function() {
+    //Declaring variables
+    const fonts = ["cursive", "san-serif", "sans", "monospace"];
+    let captchaValue = "";
+
+    function generateCaptcha(){
+        let value = btoa(Math.random()*1000000000);
+        value = value.substr(0,5+Math.random()*5);
+        captchaValue = value;
+    }
+    function setCaptcha(){
+        let html = captchaValue.split("").map((char)=>{
+            const rotate = -20 + Math.trunc(Math.random()*30);
+            const font = Math.trunc(Math.random()*fonts.length);
+            return <span
+                style="
+                    transform:rotate(${rotate}deg);
+                    font-family:${fonts[font]}
+                "
+            >${char}</span>
+        }).join("");
+        document.querySelector(".captcha .preview").innerHTML = html;
+    }
+    function initCaptcha(){
+        document.querySelector(".captcha .captcha-refresh").addEventListener("click",function(){
+            generateCaptcha();
+            setCaptcha();
+        });
+        generateCaptcha();
+        setCaptcha();
+    }
+    initCaptcha();
+
+    document.querySelector("#enter-button").addEventListener("click",function(){
+        let inputCaptchaValue = document.querySelector(".captcha input").value;
+        if(inputCaptchaValue === captchaValue){
+            swal("", "Logging In!", "success");
+        } else {
+            swal("Invalid captcha");
+        }
+    });
+})();
