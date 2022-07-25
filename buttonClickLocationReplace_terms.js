c6c5ecb9a34c136e547ac00920680a93f07671c0
Marquise Williams <marquisewms@example.com>
2022-07-24 21:06:28 -0400

    Began work on personality test
    No description

---
+++ buttonClickLocationReplace_terms.js
@@ -0,0 +1,5 @@
+(function() {
+    document.querySelector("#continue-from-terms-button").addEventListener("click",function(){
+        window.location.replace("personality_test.html");
+    });
+})();
