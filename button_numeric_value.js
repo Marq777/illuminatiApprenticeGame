fb0e21e1eea07ab0b52ca5df06f113ac245e20ba
Marquise Williams <marquisewms@example.com>
2022-07-26 14:18:08 -0400

    Working on numeric button values
    First iteration of numeric values. Not sure if it's the right way to go. Will try a ParseInt method after committing this.

---
+++ button_numeric_value.js
@@ -0,0 +1,48 @@
+(function() {
+    const onePoint = document.querySelector(".PersonalityAnswer1 input").value;     //Assign dividers into variables by points
+    const twoPoints = document.querySelector(".PersonalityAnswer2 input").value;
+    const threePoints = document.querySelector(".PersonalityAnswer3 input").value;
+    const fourPoints = document.querySelector(".PersonalityAnswer4 input").value;
+
+
+    /*function checkButtons1to4() {
+        Q1A1_true = document.getElementById('Question1').getElementsByClassName('PersonalityAnswer1')
+        Q1A1_true.checked = true;
+
+        Q1A2_true = document.getElementById('Question1').getElementsByClassName('PersonalityAnswer2')
+        Q1A2_true.checked = true;
+
+    }*/
+
+    function checkAnswer(question) {
+
+    }
+
+
+    function addValues() {
+        const onePoint = 1;
+        const twoPoints = 2;
+        const threePoints = 3;
+        const fourPoints = 4;
+
+        const total = [onePoint,twoPoints,threePoints,fourPoints]
+        totalAdded = total.reduce()
+
+        if() {
+
+        }
+    };
+    
+    });
+})();
+
+
+/* 
+
+1. Assign dividers into variables (points)
+2. Assign variables a number 1-4
+3. Put variables into an array
+4. Add all variables together
+5. Check if sum is greater than the set number to allow entry
+
+*/
